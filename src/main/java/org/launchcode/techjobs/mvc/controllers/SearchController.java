5733cc3da65905cf6221353952b3602384a4e11b
djh007ktr <djhill0531@gmail.com>
2023-03-05 17:28:43 -0600

    Complete Bonus 1

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -4,10 +4,7 @@ import org.launchcode.techjobs.mvc.models.Job;
 import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -33,11 +30,14 @@ public class SearchController {
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         if (searchTerm.toLowerCase().equals("all") || searchTerm.equals("")) {
             ArrayList<Job> jobs = JobData.findAll();
+            searchType = "all";
             model.addAttribute("jobs", jobs);
+            model.addAttribute("searchType", searchType);
         } else {
             ArrayList<Job> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
             model.addAttribute("jobs", jobs);
             model.addAttribute("columns", columnChoices);
+            model.addAttribute("searchType", searchType);
         }
         return "search";
     }
